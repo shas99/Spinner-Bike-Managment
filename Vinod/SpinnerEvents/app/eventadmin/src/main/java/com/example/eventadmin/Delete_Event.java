ce4424059f7c92548886bbcda30ff998bbdaf0f5
pasinduVinod <pasindu.vinod1999@gmail.com>
2021-08-16 20:12:18 +0530

    PasinduV Commit 1

---
+++ Vinod/SpinnerEvents/app/eventadmin/src/main/java/com/example/eventadmin/Delete_Event.java
@@ -0,0 +1,13 @@
+package com.example.eventadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Delete_Event extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_event);
+    }
+}
