a6c6cd657585a7c787f4933c205c07a538a247b6
ChathushkaRodrigo <chathushkaseniyarodrigo@protonmail.com>
2021-08-18 17:48:28 +0530

    Successfully created Spinner Login , Modifications 1. Reset Password Feature 2. Added ForgotPasswordActivity.java/xml file 3. Retrieve user data to UserProfileActivity.xml layout

--- Spinner_app/app/src/main/java/com/example/spinner/User.java
+++ Spinner_app/app/src/main/java/com/example/spinner/User.java
@@ -2,7 +2,7 @@ package com.example.spinner;
 public class User {
 
     public String name,email,sex;
-
+public User(){}
 
     public User(String name,String email,String sex){
         this.name=name;
