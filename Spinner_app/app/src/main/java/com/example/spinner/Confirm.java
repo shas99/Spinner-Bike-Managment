973f1226d303d4eb7f85c32e32bf248bc328b168
Shasvathan <pshasvathan1999@gmail.com>
2021-09-17 11:09:31 +0530

    Pages connected, and error found on Rent3 sort of fixed1

---
+++ Spinner_app/app/src/main/java/com/example/spinner/Confirm.java
@@ -0,0 +1,14 @@
+package com.example.spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Confirm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm);
+    }
+}
