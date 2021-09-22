89445ebea15a38239cc3caa2e9831ebe00130442
pasinduVinod <pasindu.vinod1999@gmail.com>
2021-09-23 01:59:13 +0530

    Final commit f

--- Spinner_app/app/src/main/java/com/example/spinner/Dashboard.java
+++ Spinner_app/app/src/main/java/com/example/spinner/Dashboard.java
@@ -1,5 +1,6 @@
 package com.example.spinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
@@ -7,6 +8,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -16,6 +26,12 @@ public class Dashboard extends AppCompatActivity {
     private RelativeLayout events;
     private RelativeLayout feedback;
     private RelativeLayout review;
+
+    DatabaseReference referenceret;
+
+    private FirebaseUser user;
+
+    private String userID;
 
 
 
@@ -72,7 +88,7 @@ public class Dashboard extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(Dashboard.this,EventsMainActivity.class);
+                Intent intent = new Intent(Dashboard.this,Event_List.class);
                 startActivity(intent);
             }
         });
@@ -88,7 +104,43 @@ public class Dashboard extends AppCompatActivity {
 
 
 
+        //to retreive information
+        referenceret= FirebaseDatabase.getInstance().getReference("UsersProfile");
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        userID =user.getUid();
+        referenceret.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile =snapshot.getValue(User.class);
 
+                if(userProfile !=null){
+                    String name =userProfile.name;
+                    String email=userProfile.email;
+                    String sex =userProfile.sex;
+                    String mobile =userProfile.mobileNo;
+                    String points = userProfile.Points;
+                    String ID = user.getUid();
+                    String Target5 = userProfile.Target;
+                    //saving userDetails
+                    UserDetails U1 = new UserDetails(name,email,sex,mobile,points,ID,Target5);
+
+
+
+
+
+
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(Dashboard.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
 
 
